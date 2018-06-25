c5b71b75f531752538499ebf61f037c84a23fded
Pachawo Bisani <phbisan@gmail.com>
2018-06-25 15:18:52 +0000

    handle exception for article not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found 
+  
+  protected
+  
+  
+  def resource_not_found 
+    message = "The article you are looking for could not be found" 
+    flash[:alert] = message
+    redirect_to root_path
+  end
+
 end
